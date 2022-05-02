451e02c4aa83429daff8593424511a55aadc7c2a
Gabriel Salinas <g.szada@gmail.com>
2022-05-02 11:12:40 +0200

    Code of stats component improved

--- src/pages/stats/stats.js
+++ src/pages/stats/stats.js
@@ -6,6 +6,7 @@ import { faGithub, faStackOverflow, faDev } from '@fortawesome/free-brands-svg-i
 import { ReactComponent as Chart } from '../../assets/SVGs/chart.svg'
 
 export default function Stats({ SO_topTech, SO_reputation }) {
+
   if (!SO_topTech || !SO_reputation) return <div>loading..</div>
 
   const points = SO_reputation.points
@@ -17,19 +18,18 @@ export default function Stats({ SO_topTech, SO_reputation }) {
     return string.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
   }
 
-  return (
-    <>
-      <div
-        style={{
-          display: 'flex'
-        }}
-      >
-        <div>
+  function Header() {
+    return (
+      <div className={s.header}>
+        <div className={s.left_box}>
           <Chart />
         </div>
-        <div style={{ marginTop: '15px' }}>
-          <h1 style={{ color: 'rgb(70, 201, 70)' }}>Fun Stats</h1>
-          <div style={{ color: '#ddd', marginRight: '20px' }}>
+
+        <div className={s.right_box}>
+          <h1 className={s.page_title}>
+            Fun Stats
+          </h1>
+          <div className={s.page_about}>
             I didn't know if I should keep this section or remove it.
             Mainly because sometimes it feels a little braggy. But
             to be honest I'm making it just for fun and give my personal
@@ -38,138 +38,118 @@ export default function Stats({ SO_topTech, SO_reputation }) {
           </div>
         </div>
       </div>
+    )
+  }
 
-
-      <div style={{ marginTop: '30px' }}>
-
-
-        <div style={{
-          display: 'flex',
-          color: 'white',
-          borderBottom: '1px solid white'
-        }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              fontSize: '40px',
-              marginLeft: '20px'
-            }}
-          >
-            <FontAwesomeIcon icon={faStackOverflow} />
-          </div>
-          <div style={{
-            paddingTop: '15px',
-            marginLeft: '15px',
-            fontSize: '23px'
-          }}
-          >
-            Stack Overflow
-          </div>
+  function Reputation() {
+    return (
+      <div className={s.stat_card} style={{ marginRight: '20px' }}>
+        <div className={s.card_title}>
+          Reputation
         </div>
-        <div className={s.flex_wrapper}>
-
-
-          <div className={s.left_box}>
-            <div
-              className={s.stat_card}
-              style={{ marginRight: '20px' }}
-            >
-              <div className={s.card_title}>
-                Reputation
-              </div>
-              <div className={s.big_number}>
-                {addThousandSeparator(points.total)}
-              </div>
-              <div className={s.flex_box1}>
-                <div>
-                  month:{' '}
-                  <FontAwesomeIcon
-                    icon={faArrowUp}
-                    style={{ color: 'green' }}
-                  />
-                  {' '}
-                  {addThousandSeparator(points.month_change)}
-                </div>
-                <div>
-                  year: {' '}
-                  <FontAwesomeIcon
-                    icon={faArrowUp}
-                    style={{ color: 'green' }}
-                  />
-                  {' '}
-                  {addThousandSeparator(points.year_change)}
-                </div>
-              </div>
-            </div>
-            <div className={s.stat_card}>
-              <div className={s.card_title}>
-                Badges
-              </div>
-              <div className={s.flex_box2}>
-                <div style={{ color: 'white', marginRight: '50px' }}>
-                  <FontAwesomeIcon icon={faTrophy} style={{ color: 'gold', opacity: '0.7' }} />
-                  {' '}
-                  {badges.gold}
-                </div>
-                <div style={{ color: 'white', marginRight: '50px' }}>
-                  <FontAwesomeIcon icon={faTrophy} style={{ color: 'silver', opacity: '0.7' }} />
-                  {' '}
-                  {badges.silver}
-                </div>
-                <div style={{ color: 'white' }}>
-                  <FontAwesomeIcon icon={faTrophy} style={{ color: 'orange', opacity: '0.7' }} />
-                  {' '}
-                  {badges.bronze}
-                </div>
-              </div>
-            </div>
+        <div className={s.big_number}>
+          {addThousandSeparator(points.total)}
+        </div>
+        <div className={s.flex_box1}>
+          <div>
+            month:{' '}
+            <FontAwesomeIcon icon={faArrowUp} style={{ color: 'green' }} />
+            {' '}
+            {addThousandSeparator(points.month_change)}
           </div>
-          <div className={s.right_box}>
-            <div className={s.tech_card}>
-              <div className={s.card_title}>
-                Top Tech
-                <div>
-                  {
-                    topTech.map((tech) => {
-                      return (
-                        <p>
-                          <div
-                            style={{
-                              display: 'flex'
-                            }}
-                          >
-                            <div
-                              style={{
-                                width: '50%',
-                                fontSize: '18px',
-                                color: 'rgba(255,255,255,0.8)'
-                              }}
-                            >
-                              {tech.tag_name}
-                            </div>
-                            <div
-                              style={{
-                                width: '50%',
-                                textAlign: 'right',
-                                color: 'white'
-                              }}
-                            >
-                              {tech.answer_score}
-                            </div>
-                          </div>
-                        </p>
-                      )
-                    })
-                  }
-                </div>
-              </div>
-            </div>
+          <div>
+            year: {' '}
+            <FontAwesomeIcon icon={faArrowUp} style={{ color: 'green' }} />
+            {' '}
+            {addThousandSeparator(points.year_change)}
           </div>
         </div>
       </div>
+    )
+  }
 
+  function Badges() {
+    return (
+      <div className={s.stat_card}>
+        <div className={s.card_title}>
+          Badges
+        </div>
+        <div className={s.flex_box2}>
+          <div style={{ marginRight: '50px' }}>
+            <FontAwesomeIcon icon={faTrophy} className={s.gold} />
+            {' '}
+            {badges.gold}
+          </div>
+          <div style={{ marginRight: '50px' }}>
+            <FontAwesomeIcon icon={faTrophy} className={s.silver} />
+            {' '}
+            {badges.silver}
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faTrophy} className={s.bronze} />
+            {' '}
+            {badges.bronze}
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  function TopTech() {
+    return (
+      <div className={s.tech_card}>
+        <div className={s.card_title}>
+          Top Tech
+        </div>
+        <div>
+          {
+            topTech.map((tech) => {
+              return (
+                <div className={s.tag_box}>
+                  <div className={s.name}>
+                    {tech.tag_name}
+                  </div>
+                  <div className={s.score}>
+                    {tech.answer_score}
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Header />
+
+      {/* stack overflow stats */}
+      <div id={s.stack_overflow}>
+
+        <div className={s.header}>
+          <div className={s.icon}>
+            <FontAwesomeIcon icon={faStackOverflow} />
+          </div>
+          <div className={s.title}>
+            Stack Overflow
+          </div>
+        </div>
+
+        <div className={s.body}>
+          <div className={s.left_box}>
+            <Reputation />
+            <Badges />
+          </div>
+          <div className={s.right_box}>
+            <TopTech />
+          </div>
+        </div>
+
+      </div>
+
+      {/* github stats */}
       <div style={{
         display: 'flex',
         color: 'white',
@@ -197,7 +177,7 @@ export default function Stats({ SO_topTech, SO_reputation }) {
         </div>
       </div>
 
-
+      {/* devto stats */}
       <div style={{
         display: 'flex',
         color: 'white',
