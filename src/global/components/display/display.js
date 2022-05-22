835b370a7303ff9fd17c53dca4786c6ac4ebf415
Gabriel Salinas <g.szada@gmail.com>
2022-05-22 11:00:11 +0200

    Improved styles and added icons to display component

--- src/global/components/display/display.js
+++ src/global/components/display/display.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import s from './display.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStackOverflow, faDev } from '@fortawesome/free-brands-svg-icons'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { ReactComponent as Codepen } from '../../../global/assets/SVGs/codepen.svg'
+import { ReactComponent as Github } from '../../../global/assets/SVGs/github.svg'
+import { ReactComponent as Twitter } from '../../../global/assets/SVGs/twitter.svg'
 
 export default function Display() {
   return (
@@ -10,11 +16,25 @@ export default function Display() {
       </div>
 
       <div className={s.screen}>
+
+        <div className={s.social}>
+          <div className={s.button_box}>
+            <FontAwesomeIcon icon={faCaretRight} id={s.arrow} />
+          </div>
+          <div className={s.icons_wrapper}>
+            <Github id={s.git} />
+            <FontAwesomeIcon icon={faStackOverflow} id={s.stack} />
+            <Twitter id={s.twitter} />
+            <Codepen id={s.codepen} />
+          </div>
+        </div>
+
         <div className={s.msg_display}>
           <div className={s.scroll_text}>
             message test
           </div>
         </div>
+
       </div>
 
     </section>
