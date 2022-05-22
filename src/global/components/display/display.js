3f211f92f14861027d3256b5bebf4d065d6143dc
Gabriel Salinas <g.szada@gmail.com>
2022-05-22 12:18:44 +0200

    social box mechanism is now working

--- src/global/components/display/display.js
+++ src/global/components/display/display.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './display.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStackOverflow, faDev } from '@fortawesome/free-brands-svg-icons'
@@ -6,8 +6,44 @@ import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { ReactComponent as Codepen } from '../../../global/assets/SVGs/codepen.svg'
 import { ReactComponent as Github } from '../../../global/assets/SVGs/github.svg'
 import { ReactComponent as Twitter } from '../../../global/assets/SVGs/twitter.svg'
+import useSound from 'use-sound'
+import bounce from '../../../global/assets/sounds/bounce.wav'
+import unfa from '../../../global/assets/sounds/unfa_select.flac'
 
 export default function Display() {
+  const ini = { pixels: '270px', degrees: '0deg' }
+  const [pixels, setPixels] = useState(ini.pixels)
+  const [degrees, setDegrees] = useState(ini.degrees)
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [playSound] = useSound(bounce, { volume: 0.5, playbackRate: 2 })
+  const [playSoundTwo] = useSound(unfa)
+  const links = {
+    SO: `https://stackoverflow.com/users/14895985/gass?tab=profile`,
+    github: `https://github.com/gass-git`,
+    twitter: `https://twitter.com/gass_tweets`,
+    sandbox: `https://codesandbox.io/u/g.szada`
+  }
+
+  function expand() {
+    if (pixels !== ini.pixels) {
+      playSound()
+      setDegrees(ini.degrees)
+      setPixels(ini.pixels)
+      setIsCollapsed(false)
+    }
+    else {
+      playSound()
+      setDegrees('180deg')
+      setPixels('33px')
+      setTimeout(() => setIsCollapsed(true), 200)
+    }
+  }
+
+  function handleClick(site) {
+    window.open(links[site], '_blank')
+    playSoundTwo()
+  }
+
   return (
     <section id={s.display}>
 
@@ -16,16 +52,41 @@ export default function Display() {
       </div>
 
       <div className={s.screen}>
-
-        <div className={s.social}>
-          <div className={s.button_box}>
-            <FontAwesomeIcon icon={faCaretRight} id={s.arrow} />
+        <div
+          className={s.social}
+          style={{ width: `${pixels}` }}
+        >
+          <div
+            className={s.button_box}
+            onClick={() => expand()}
+          >
+            <FontAwesomeIcon
+              icon={faCaretRight}
+              id={s.arrow}
+              style={{ transform: `rotate(${degrees})` }}
+            />
           </div>
-          <div className={s.icons_wrapper}>
-            <Github id={s.git} />
-            <FontAwesomeIcon icon={faStackOverflow} id={s.stack} />
-            <Twitter id={s.twitter} />
-            <Codepen id={s.codepen} />
+          <div
+            className={s.icons_wrapper}
+            style={isCollapsed ? { display: `none` } : null}
+          >
+            <Github
+              id={s.git}
+              onClick={() => handleClick(`github`)}
+            />
+            <FontAwesomeIcon
+              icon={faStackOverflow}
+              id={s.stack}
+              onClick={() => handleClick(`SO`)}
+            />
+            <Twitter
+              id={s.twitter}
+              onClick={() => handleClick(`twitter`)}
+            />
+            <Codepen
+              id={s.codepen}
+              onClick={() => handleClick(`sandbox`)}
+            />
           </div>
         </div>
 
