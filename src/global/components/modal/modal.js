ceac71bd43f8e5d505472e889f5d1c7c573feb78
Gabriel Salinas <75760163+gass-git@users.noreply.github.com>
2022-05-28 18:36:48 +0200

    Modal component improved

--- src/global/components/modal/modal.js
+++ src/global/components/modal/modal.js
@@ -3,37 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import s from './modal.module.css'
 
-export default function Modal({ children, showModal, setShowModal, scrollPos }) {
+export default function Modal({ 
+  title,
+  children, 
+  showModal, 
+  setShowModal, 
+  scrollPos
+  }) {
   const [scrollHeight, setScrollHeight] = useState(0)
 
   useEffect(() => {
     setScrollHeight(document.documentElement.scrollHeight)
   }, [])
 
-  /*console.log(scrollPos)*/
-
   if (showModal) {
     return (
       <section id={s.modal}>
-        <div className={s.back}
-          style={{
-            height: `${scrollHeight}px`
-          }}
+        <div 
+          className={s.backdrop}
+          style={{ height: `${scrollHeight}px` }}
           onClick={() => setShowModal(false)}
-        >
-        </div>
+        />
         <div
           className={s.card}
-          style={{
-            top: `${scrollPos + 100}px`
-          }}
+          style={{ top: `${scrollPos + 100}px`}}
         >
           <div className={s.header}>
-            <FontAwesomeIcon
-              icon={faXmark}
-              onClick={() => setShowModal(false)}
-            />
-            title
+            <div className={s.title}>
+              {title}
+            </div>
+            <div className={s.Xmark_box}>
+              <FontAwesomeIcon
+                icon={faXmark}
+                onClick={() => setShowModal(false)}
+              />
+            </div>
           </div>
           <div className={s.content}>
             {children}
